9ad6ba9ce30cf27fc872f242e946ea569d4e7399
Miroslav Lysyuk <mirolysyuk@gmail.com>
2021-04-05 22:01:35 -0500

    reconiguring number of tweets

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Twit = require('twit');
+const config = require('./config.js');
+const tweetBot = new Twit(config);
+
+const params = {
+    q: '#generativeart  filter:media',
+    count: 1,
+    lang: 'en'
+}
+
+const favParams = {
+    q: '#generativeart  filter:media',
+    count: 1,
+    lang: 'en'
+}
+
+tweetBot.get('search/tweets', params,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet successful`)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', favParams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Favorite successful`)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
